5058cd2205ea9de5499c359b3cc8cea5426a5291
ibare <ibare77@gmail.com>
2015-05-10 00:03:03 +0900

    init codemirror

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('static'));
+app.use(express.static('bower_components/'));
+
 app.listen(8080, function() {
   console.log('Running');
 });
